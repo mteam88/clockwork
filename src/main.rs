d0c57eb2d7680cbcca07877b91ff67418538fa5d
Matthew <matthew8edelen@gmail.com>
2024-06-19 15:39:27 -0400

    buying shares

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use alloy::{
-    primitives::address,
-    providers::{Provider, ProviderBuilder, WsConnect},
-    rpc::types::{BlockNumberOrTag, Filter},
+    network::EthereumWallet, primitives::{address, utils::parse_ether, Uint}, providers::{Provider, ProviderBuilder, WsConnect}, rpc::types::{BlockNumberOrTag, Filter}, signers::local::PrivateKeySigner, sol
 };
 use eyre::Result;
 use futures_util::stream::StreamExt;
 use dotenv::dotenv;
 
+// Codegen from ABI file to interact with the contract.
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    TimeBasedExperience,
+    "abi/TimeBasedExperience.json"
+);
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
 
-    let _private_key = dotenv::var("PRIVATE_KEY")?;
+    let private_key = dotenv::var("PRIVATE_KEY")?;
+    let signer: PrivateKeySigner = private_key.parse().expect("Invalid private key");
+    let wallet = EthereumWallet::new(signer);
 
     // Set up the WS transport which is consumed by the RPC client.
     let rpc_url = dotenv::var("RPC_URL")?;
 
     // Create the provider.
     let ws = WsConnect::new(rpc_url);
-    let provider = ProviderBuilder::new().on_ws(ws).await?;
+    let provider = ProviderBuilder::new().with_recommended_fillers().wallet(wallet).on_ws(ws).await?;
 
     // Create a filter to watch for any SharesCreated events.
     let timefun_address = address!("428aeF7fB31E4E86162D62d4530a4dd7232D953D");
@@ -37,6 +45,16 @@ async fn main() -> Result<()> {
 
     while let Some(log) = stream.next().await {
         println!("timefun event: {log:?}");
+        let TimeBasedExperience::SharesCreated { creator, referrer } = log.log_decode()?.inner.data;
+        println!("Creator: {creator}, Referrer: {referrer}");
+
+        println!("Buying shares...");
+        let timefun = TimeBasedExperience::new(timefun_address, provider.clone());
+        let tx = timefun.buyShares(creator, Uint::from(10)).value(parse_ether("0.008").unwrap());
+        
+        let receipt = tx.send().await?.with_required_confirmations(1).with_timeout(Some(std::time::Duration::from_secs(10))).watch().await?;
+
+        println!("Transaction hash: {receipt}");
     }
 
     Ok(())
